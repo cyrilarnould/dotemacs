ea7977e3678cc90a4a2ea07bf79c081787394119
Cyril Arnould <cyril.arnould@outlook.com>
2023-04-18 18:24:46 +0200

    Update msys_setup
    * Add msys packages
    * Add python packages

--- msys_setup.sh
+++ msys_setup.sh
@@ -1,8 +1,33 @@
 # Update before installing
 pacman -Syu
 pacman -Su
-# Install ucrt packages
-pacman -S mingw-w64-ucrt-x86_64-make mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-cmake
-pacman -S mingw-w64-ucrt-x86_64-grep mingw-w64-ucrt-x86_64-diffutils mingw-w64-ucrt-x86_64-ghdl-llvm mingw-w64-ucrt-x86_64-tidy \
-mingw-w64-ucrt-x86_64-libgccjit mingw-w64-ucrt-x86_64-ghostscript mingw-w64-ucrt-x86_64-emacs-pdf-tools-server \
-mingw-w64-ucrt-x86_64-hunspell mingw-w64-ucrt-x86_64-hunspell-en mingw-w64-ucrt-x86_64-gdb mingw-w64-ucrt-x86_64-emacs
+# Install msys packages
+pacman -S \
+       base-devel \
+       dejagnu \
+       git \
+       vim
+# Install development packages
+pacman -S \
+       mingw-w64-ucrt-x86_64-make \
+       mingw-w64-ucrt-x86_64-gcc \
+       mingw-w64-ucrt-x86_64-cmake \
+       mingw-w64-ucrt-x86_64-gdb \
+       mingw-w64-ucrt-x86_64-python-pip \
+       mingw-w64-ucrt-x86_64-python-numpy
+# Install emacs packages
+pacman -S \
+       mingw-w64-ucrt-x86_64-emacs \      
+       mingw-w64-ucrt-x86_64-grep \
+       mingw-w64-ucrt-x86_64-diffutils \
+       mingw-w64-ucrt-x86_64-ghdl-llvm \
+       mingw-w64-ucrt-x86_64-tidy \
+       mingw-w64-ucrt-x86_64-libgccjit \
+       mingw-w64-ucrt-x86_64-ghostscript \
+       mingw-w64-ucrt-x86_64-emacs-pdf-tools-server \
+       mingw-w64-ucrt-x86_64-hunspell \
+       mingw-w64-ucrt-x86_64-hunspell-en \
+       mingw-w64-ucrt-x86_64-python-pylint 
+# Install miscellaneous packages
+pacman -S \
+       mingw-w64-ucrt-x86_64-gnuplot
